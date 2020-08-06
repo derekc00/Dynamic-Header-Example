8575f35b3829267b50f1d2154f52eaf89cbb2ab2
Derek Chang <derektc1@uci.edu>
2020-08-05 22:47:03 -0700

    Initial Commit

---
+++ Dynamic Header Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dynamic Header Example
+//
+//  Created by Derek Chang on 8/5/20.
+//  Copyright © 2020 Derek Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
